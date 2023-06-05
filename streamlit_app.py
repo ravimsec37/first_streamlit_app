390764c9dc503b917d25b4ff9b29d20e4b53b2cf
Ravichander Rajendran <56529331+ravimsec37@users.noreply.github.com>
2023-06-05 12:45:23 +0530

    Update streamlit_app.py
    Added few more changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,4 @@
 import streamlit
 streamlit.title('All is Well')
+streamlit.header('Everything in life happens for some good reason')
+streamlit.text('It is believed that everything happens in life is for some good reason, it is a belief and a motivation to make our day much happier and more positive')
